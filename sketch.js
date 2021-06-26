8cb60c7c655d8275b9038da5a548a336642307b0
VanshikaPakki123 <76252285+VanshikaPakki123@users.noreply.github.com>
2021-06-26 14:25:58 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,44 +1,55 @@
-var canvas, backgroundImage;
-
-var gameState = 0;
-var playerCount;
-var allPlayers;
-var distance = 0;
 var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+var score =0;
+var player, form,game;
+var player1,player2;
+var players;
+var fruits;
 
-var form, player, game;
-
-var cars, car1, car2, car3, car4;
-
-var track, car1_img, car2_img, car3_img, car4_img;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+var player1score =0;
+var player2score =0;
 
 function preload(){
-  track = loadImage("images/track.jpg");
-  car1_img = loadImage("images/car1.png");
-  car2_img = loadImage("images/car2.png");
-  car3_img = loadImage("images/car3.png");
-  car4_img = loadImage("images/car4.png");
-  ground = loadImage("images/ground.png");
-}
+  back_img = loadImage("images/jungle.jpg");
+  player_img = loadImage("images/basket2.png");
+  fruit1_img = loadImage("images/apple2.png");
+  fruit2_img = loadImage("images/banana2.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/orange2.png");
+  fruit5_img = loadImage("images/pineapple2.png");
+  fruitGroup = new Group();
 
-function setup(){
-  canvas = createCanvas(displayWidth - 20, displayHeight-30);
+  
+}
+function setup() {
+  createCanvas(1000, 600);
   database = firebase.database();
   game = new Game();
   game.getState();
   game.start();
+  
+  
+  
 }
 
+function draw() {
+  background(back_img);
 
-function draw(){
-  if(playerCount === 4){
-    game.update(1);
-  }
-  if(gameState === 1){
-    clear();
-    game.play();
-  }
-  if(gameState === 2){
-    game.end();
-  }
+   if (playerCount === 2) {
+     game.update(1);
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+    
+     game.end();
+   }
 }
